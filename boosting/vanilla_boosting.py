4b349b8365a143712a0b2e9383a6eb35f2b26074
Fernando Ramacciotti <fernandoramacciotti@gmail.com>
2020-06-13 18:19:17 -0300

    fixed vanilla-boosting predictions shape

--- boosting/vanilla_boosting.py
+++ boosting/vanilla_boosting.py
@@ -3,10 +3,11 @@ from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 
 
 class VanillaBoosting(object):
-    def __init__(self, n_iter=10, alpha=0.1, max_depth=4):
+    def __init__(self, n_iter=10, alpha=0.1, max_depth=4, verbose=True):
         self.n_iter = n_iter
         self.alpha = alpha
         self.max_depth = max_depth
+        self.verbose = verbose
 
     def fit(self, X, y=None):
         # init first weak learner
@@ -29,23 +30,34 @@ class VanillaBoosting(object):
             # MSE
             mse = np.mean(np.sqrt(np.sum(np.power(y - H, 2))))
             self.mse_.append(mse)
-            print(f"Iter {iter+1}: MSE = {mse:.5f}")
+            if self.verbose:
+                print(f"Iter {iter+1}: MSE = {mse:.5f}")
 
         self.booster_ = H
         return self
 
     def predict(self, X):
-        preds = [self.alpha * self.weak_learners_[iter].predict(X)
-                for iter in range(self.n_iter)]
-        pred = np.sum(preds)
+        preds = [
+            self.alpha * self.weak_learners_[iter].predict(X)
+            for iter in range(self.n_iter)
+        ]
+        pred = np.sum(preds, axis=0)
         return pred
 
 
 if __name__ == "__main__":
-    from sklearn.datasets import make_classification
+    from sklearn.datasets import make_regression
+    import matplotlib.pyplot as plt
 
-    X, y = make_classification(random_state=42)
+    X, y = make_regression(random_state=42)
     boost = VanillaBoosting()
     boost.fit(X, y)
     preds = boost.predict(X)
-    print(preds)
+
+    # plot
+    plt.figure(figsize=(6, 3))
+    plt.scatter(preds, y)
+    plt.title("Predictions vs Real")
+    plt.xlabel("Pred")
+    plt.ylabel("Tgt")
+    plt.show()
